39c8325dcf0db29fcb1aee5be0610008bb2a9026
Vectari <mateusz.majer996@gmail.com>
2024-04-05 01:43:38 +0200

    Panel concept

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,3 +1,17 @@
 export function Panel() {
-  return <h1>Test Panel</h1>;
+  return (
+    <>
+      <h1>Daily Routine</h1>
+      <input type="text" />
+      <button>Add task</button>
+      <p>Task 1</p>
+      <input type="checkbox"></input>
+      <p>Task 2</p>
+      <input type="checkbox"></input>
+      <p>Task 3</p>
+      <input type="checkbox"></input>
+      <p>Task 4</p>
+      <input type="checkbox"></input>
+    </>
+  );
 }
