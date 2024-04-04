7a59ed42b43ae43a171a3245631171823b01c0cf
Vectari <mateusz.majer996@gmail.com>
2024-04-05 01:33:32 +0200

    add styled components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,14 @@
+import styled from "styled-components";
 import { useState, useEffect } from "react";
+
+const StyledLi = styled.li`
+  padding: 20px;
+  margin: 20px auto;
+  border-radius: 30px;
+  max-width: 200px;
+  list-style-type: none;
+  background-color: aliceblue;
+`;
 
 const url = "http://localhost:3000/tasks";
 
@@ -18,26 +28,30 @@ function App() {
       });
   }, []);
 
-  console.log(data)
 
   return (
     <>
       <h1>Test</h1>
       <ul>
-        {data.map(task => (
+        {data.map((task) =>
           // Using Object.entries to access the date and status
           Object.entries(task).map(([date, details]) => (
-            <li key={details.id}>
-              Date: {new Date(date).toLocaleDateString()}<br />
-              {Object.entries(details).map(([key, value]) => (
+            <StyledLi key={details.id}>
+              {details.length}
+              Date: {new Date(date).toLocaleDateString()}
+              <br />
+              {Object.entries(details).map(([key, value]) =>
                 // Render only if the value is "done"
-                value === 'done' ? (
-                  <span key={key}>{key}: {value}<br /></span>
+                value === "done" ? (
+                  <span key={key}>
+                    {key}: {value}
+                    <br />
+                  </span>
                 ) : null
-              ))}
-            </li>
+              )}
+            </StyledLi>
           ))
-        ))}
+        )}
       </ul>
     </>
   );
